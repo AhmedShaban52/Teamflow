b46780069a0254cd0f63a4d468ee5c96562499d0
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2025-11-15 07:49:32 +0200

    feat: add message editing functionality with hover toolbar and update message API

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/message/MessageItem.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/message/MessageItem.tsx
@@ -2,12 +2,17 @@ import { SafeContent } from "@/components/rich-text-editor/SafeContent";
 import { Message } from "@/lib/generated/prisma/client";
 import { getAvatar } from "@/lib/get-avatar";
 import Image from "next/image";
+import { MessageHoverToolbar } from "../toolbar";
+import { useState } from "react";
+import { EditMessage } from "../toolbar/EditMessage";
 
 interface iAppProps {
     message: Message
+    currentUserId: string
 }
 
-export function MessageItem({ message }: iAppProps) {
+export function MessageItem({ message, currentUserId }: iAppProps) {
+    const [isEditing, setIsEditing] = useState(false)
     return (
         <div className="flex space-x-3 relative p-3 rounded-lg group hover:bg-muted/50">
             <Image
@@ -36,17 +41,29 @@ export function MessageItem({ message }: iAppProps) {
                     </p>
                 </div>
 
-                <SafeContent
-                    content={JSON.parse(message.content)}
-                    className="text-sm wrap-break-word prose dark:prose-invert max-w-none marker:text-primary"
-                />
+                {isEditing ? (
+                    <EditMessage message={message} onCancel={() => setIsEditing(false)} onSave={() => setIsEditing(false)}/>
+                ) : (
+                    <>
+                        <SafeContent
+                            content={JSON.parse(message.content)}
+                            className="text-sm wrap-break-word prose dark:prose-invert max-w-none marker:text-primary"
+                        />
 
-                {message.imageUrl && (
-                    <div className="mt-3">
-                        <Image src={message.imageUrl} alt="Message Attachment" width={512} height={512} className="rounded-md max-h-[320px] w-auto object-contain" />
-                    </div>
+                        {message.imageUrl && (
+                            <div className="mt-3">
+                                <Image src={message.imageUrl} alt="Message Attachment" width={512} height={512} className="rounded-md max-h-[320px] w-auto object-contain" />
+                            </div>
+                        )}
+                    </>
                 )}
             </div>
+
+            <MessageHoverToolbar
+                messageId={message.id}
+                canEdit={message.authorId === currentUserId}
+                onEdit={() => setIsEditing(true)}
+            />
         </div>
     )
 }
